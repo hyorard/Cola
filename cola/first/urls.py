93ca433f56bc2846b1d9c85f9397345dff48f70a
jhn5052 <jhn5052@github.com>
2019-07-17 23:40:53 +0900

    add board

---
+++ cola/first/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('',views.main, name="main"),
+    path('board/',views.board, name="board"),
+    path('board/<int:board_id>', views.detail, name="detail"),
+    path('board/new/',views.new, name="new"),
+    path('board/create', views.create, name="create"),
+]
--- cola/first/views.py
+++ cola/first/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Board
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+def board(request):
+    boards = Board.objects
+    return render(request, 'board.html', {'boards':boards})
+
+def detail(request, board_id):
+    board_detail = get_object_or_404(Board, pk = board_id)
+    return render(request, 'detail.html', {'board':board_detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    board = Board()
+    board.title = request.GET['title']
+    board.body = request.GET['body']
+    board.pub_date = timezone.datetime.now()
+    board.save()
+    return redirect('/board/'+str(board.id))
