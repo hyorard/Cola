0de10b9c1f3cbf3cd3cf5153de7dac2969238e8a
hyoriezmann <reddream09@naver.com>
2019-07-27 15:09:14 +0900

    diff myRecent - minjiRecent

--- cola/teamproject/views.py
+++ cola/teamproject/views.py
@@ -53,31 +53,58 @@ def teamInfo(request):
         return render(request, 'teamInfo.html', {'team':team, 'members':members})
 
 '''
-변경할 때 몇 개 안 적어놓고 변경하면 오류 뜸 디버깅 ㄱㄱ
+지난 시간 -> 남은 시간으로 구현 변경 ㄱㄱ
+지난 시간 기능도 안하고 있음
 '''
 def changeTeamInfo(request):
+    # 팀 정보 창에서 변경 요청 시
     try:
         request.POST['type']
         teamId = request.POST['teamId']
         team = Team.objects.get(id=teamId)
         return render(request, 'changeTeamInfo.html', {'team':team})
+    # 팀 정보 변경 창에서 정보 입력하고 변경시
+    # try -> 정보 입력했으면 변경
+    # except -> 정보 미입력했으면 pass
     except:
         teamId = request.POST['teamId']
         team = Team.objects.get(id=teamId)
-        team.name = request.POST['teamName']
+
+        try:
+            team.name = request.POST['teamName']
+        except:
+            pass
+
         # 리더 변경 ? : team.leader
         # 팀원 추방 ? : team.members ( manytomany )
 
-        strDeadline = request.POST['deadline']
-        dateDeadline = datetime.datetime.strptime(strDeadline, "%Y-%m-%d").date()
-        team.deadline = dateDeadline
+        try:
+            strDeadline = request.POST['deadline']
+            dateDeadline = datetime.datetime.strptime(strDeadline, "%Y-%m-%d").date()
+            team.deadline = dateDeadline
+        except:
+            pass
 
-        team.progress = request.POST['progress']
-        if request.POST['is_finished'] == 'finished':
-            team.is_finished = True
-        else:
-            team.is_finished = False
-        team.save()
+        try:
+            team.progress = request.POST['progress']
+        except:
+            pass
+
+        try:
+            if request.POST['is_finished'] == 'finished':
+                team.is_finished = True
+            else:
+                team.is_finished = False
+        except:
+            pass
+
+        try:
+            team.save()
+        except:
+            pass
+        
+        teamId = request.POST['teamId']
+        team = Team.objects.get(id=teamId)
         members = team.showMembers()
         return render(request, 'teamInfo.html', {'team':team, 'members':members})
 
