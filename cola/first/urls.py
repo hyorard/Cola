69dce5f9b7db57719b6a623554b0cb212d31e38d
hyoriezmann <reddream09@naver.com>
2019-08-10 01:02:09 +0900

    first/urls.py main deleted;

--- cola/first/urls.py
+++ cola/first/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.main, name="main"),
     path('board/',views.board, name="board"),
     path('board/info',views.infoboard, name="infoboard"),
     path('board/info/searchPost',views.searchPost, name="searchPost"),
@@ -14,7 +13,9 @@ urlpatterns = [
     path('board/changeComment', views.changeComment, name='changeComment'),
     path('board/new/',views.new, name="new"),
     path('board/create2', views.create, name="create"),
+    path('board/modify', views.modify, name="modify"),
     path('board/modify/<int:board_id>', views.modify, name="modify"),
+    path('board/modifyAction', views.modifyAction, name="modifyAction"),
     path('board/modifyAction/<int:board_id>', views.modifyAction, name="modifyAction"),
     path('board/removeBoard/<int:board_id>', views.removeBoard, name="removeBoard"),
 
